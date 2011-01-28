fc11e2a32464f9e71090c59bad781674ffa1dc56
Felix Becker <git@felixbecker.name>
2011-01-29 00:18:26 +0100

    first handshake (visualizer)

--- src/main/scala/de/tdng2011/game/sampleclient/Client.scala
+++ src/main/scala/de/tdng2011/game/sampleclient/Client.scala
@@ -5,6 +5,8 @@ import de.tdng2011.game.library.{Player, Shot}
 import de.tdng2011.game.library.util.StreamUtil
 import visual.Visualizer
 import java.io.DataInputStream
+import java.nio.ByteBuffer
+import java.nio.ByteBuffer._
 
 object Client {
   val playerType = 0
@@ -18,7 +20,8 @@ object Client {
 
   def main(args : Array[String]){
 
-    //val connection = new Socket("localhost",1337);
+    connection.getOutputStream.write(ByteUtil.toByteArray(1.shortValue));
+
 
     val stream = new DataInputStream(connection.getInputStream)
 
@@ -51,5 +54,31 @@ object Client {
       }
     }
     connection
+  }
+}
+
+
+
+object ByteUtil {
+
+  def toByteArray(a : Any*) : Array[Byte] = {
+    val byteBuffer : ByteBuffer = allocate(a.size*8) // pessimistic size, works if all elements are 8 bytes
+    for(x <- a){
+      x match {
+        case x : Float => byteBuffer.putFloat(x)
+        case x : Double => byteBuffer.putDouble(x)
+        case x : Long => byteBuffer.putLong(x)
+        case x : Int => byteBuffer.putInt(x)
+        case x : Short => byteBuffer.putShort(x)
+        case x : Char => byteBuffer.putChar(x)
+        case x : Byte => byteBuffer.put(x)
+        case x : Boolean => byteBuffer.put(if(x) 1.byteValue else 0.byteValue)
+        case barbraStreisand => println("error! unknown value, your byte array will not contain " + barbraStreisand)
+      }
+    }
+    val byteArray = new Array[Byte](byteBuffer.position)
+    byteBuffer.position(0)
+    byteBuffer.get(byteArray)
+    byteArray
   }
 }
