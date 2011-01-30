7ec4c5977179e88fdbecf5b96cf0c77106a961af
Benjamin Neff <benjamin@coding4coffee.ch>
2011-01-30 23:08:07 +0100

    rename stream

--- src/main/scala/de/tdng2011/game/sampleclient/Client.scala
+++ src/main/scala/de/tdng2011/game/sampleclient/Client.scala
@@ -12,17 +12,17 @@ object Client {
   handshakePlayer
 
   def main(args : Array[String]){
-    val stream = new DataInputStream(connection.getInputStream)
+    val iStream = new DataInputStream(connection.getInputStream)
 
     connection.getOutputStream.write(ByteUtil.toByteArray(true, false, true, false))
 
     while(true){
-      val buf = StreamUtil.read(stream, 2)
+      val buf = StreamUtil.read(iStream, 2)
       val id = buf.getShort
 
       id match {
-        case x if x == EntityTypes.Player.id => entityList = entityList :+ new Player(stream)
-        case x if x == EntityTypes.Shot.id   => entityList = entityList :+ new Shot(stream)
+        case x if x == EntityTypes.Player.id => entityList = entityList :+ new Player(iStream)
+        case x if x == EntityTypes.Shot.id   => entityList = entityList :+ new Shot(iStream)
         case x if x == EntityTypes.World.id  => {
           entityList = List[Any]()
         }
@@ -36,7 +36,7 @@ object Client {
 
   def handshakePlayer =  {
     connection.getOutputStream.write(ByteUtil.toByteArray(0.shortValue, "123456789012"))
-    val response = StreamUtil.read(new DataInputStream(connection.getInputStream), 9);
+    val response = StreamUtil.read(new DataInputStream(connection.getInputStream), 9)
     println("response code: " + response.get)
     println("publicId: " + response.getLong)
   };
